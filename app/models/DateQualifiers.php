0c1d20e07718fd7cbbf71549e3defdb5b23c96fd
unknown <dpett@.local>
2011-05-31 17:30:32 +0100

    First commit

---
+++ app/models/DateQualifiers.php
@@ -0,0 +1,42 @@
+<?php
+/**
+* @category Zend
+* @package Db_Table
+* @subpackage Abstract
+* 
+* @author Daniel Pett dpett @ britishmuseum.org
+* @copyright 2010 - DEJ Pett
+* @license GNU General Public License
+* @todo add caching
+*/
+class DateQualifiers extends Zend_Db_Table_Abstract {
+
+	protected $_name = 'datequalifiers';
+	
+	protected $_primaryKey = 'id';
+
+	protected $_cache = NULL;
+	
+	/** Construct the cache object
+	* @return object
+	*/
+	
+	public function init() {
+	$this->_cache = Zend_Registry::get('rulercache');
+	}
+
+	/** Get qualifier terms for form listing as key value pairs
+	* @return array
+	*/
+	
+	public function getTerms(){
+        if (!$options = $this->_cache->load('circa')) {
+	    $select = $this->select()
+                       ->from($this->_name, array('id', 'term'))
+                       ->order('id');
+        $options = $this->getAdapter()->fetchPairs($select);
+		$this->_cache->save($options, 'circa');
+		}
+        return $options;
+    }
+}
