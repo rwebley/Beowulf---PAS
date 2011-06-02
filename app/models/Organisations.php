56e0eb710d5c54c2d46d291419699abfa6db44fa
unknown <dpett@.local>
2011-06-02 14:08:37 +0100

    Organisational files added and added OS data models for first commit

--- app/models/Organisations.php
+++ app/models/Organisations.php
@@ -1,4 +1,14 @@
 <?php
+/**
+* @category Zend
+* @package Db_Table
+* @subpackage Abstract
+* 
+* @author Daniel Pett dpett @ britishmuseum.org
+* @copyright 2010 - DEJ Pett
+* @license GNU General Public License
+* @todo add edit and delete functions
+*/
 class Organisations extends Zend_Db_Table_Abstract {
 
 	protected $_name = 'organisations';
