292ddc071e20e68f53ec86a29e9d3e12475eb33a
unknown <dpett@.local>
2011-06-09 15:22:05 +0100

    Added IA stuff

---
+++ app/models/TvcDatesToCases.php
@@ -0,0 +1,78 @@
+<?php
+/**
+* Data model for accessing treasure valuation dates and cases from link table
+* @category Zend
+* @package Db_Table
+* @subpackage Abstract
+* 
+* @author Daniel Pett dpett @ britishmuseum.org
+* @copyright 2010 - DEJ Pett
+* @license GNU General Public License
+* @version 1
+* @since 22 October 2010, 17:12:34
+* @todo implement edit and delete function methods
+*/
+class TvcDatesToCases extends Zend_Db_Table_Abstract {
+	
+	protected $_primary = 'id';
+	
+	protected $_name = 'tvcDatesToCases';
+	
+	protected $_cache, $_time, $_treasureID, $_request;
+	
+	/** Construct the auth and cache objects
+	* @return object
+	*/
+	public function init() {
+		$this->_cache = Zend_Registry::get('rulercache');
+		$this->_request = Zend_Controller_Front::getInstance()->getRequest();
+		$this->_treasureID = Zend_Controller_Front::getInstance()->getRequest()->getParam('treasureID');
+		$this->_time = Zend_Date::now()->toString('yyyy-MM-dd HH:mm:ss');
+		$this->_auth = Zend_Registry::get('auth');
+	}
+	
+	/** Add new TVC date/case link to database
+	* @param array $data
+	* @return boolean
+	*/
+	public function add($data){
+	if (!isset($data['created']) || ($data['created'] instanceof Zend_Db_Expr)) {
+    $data['created'] = $this->_time;
+  	}
+  	$data['treasureID'] = $this->_treasureID;
+  	$data['createdBy'] = $this->_auth->getIdentity()->id;
+	return parent::insert($data);
+	}
+	
+	/** List cases for a TVC
+	* @param integer $tvcID the tvc id number
+	* @return array
+	*/	
+	public function listCases($tvcID){
+	$tvcs = $this->getAdapter();
+	$select = $tvcs->select()
+		->from($this->_name,array('treasureID'))
+		->joinLeft('tvcDates',$this->_name . '.tvcID = tvcDates.secuid', array())
+		->where('tvcDates.id = ?',$tvcID)
+		->order($this->_name . '.' . $this->_primary);
+	$data =  $tvcs->fetchAll($select);
+	return $data;
+	}
+	
+	/** List dates for a TVC case
+	* @param integer $treasureID the tvc id number
+	* @return array
+	*/	
+	public function listDates($treasureID){
+	$tvcs = $this->getAdapter();
+	$select = $tvcs->select()
+			->from($this->_name,array())
+			->joinLeft('tvcDates',$this->_name . '.tvcID = tvcDates.secuid', array('id','date','location'))
+			->where($this->_name . '.treasureID = ?',$treasureID)
+			->order($this->_name . '.' . $this->_primary);
+	$data =  $tvcs->fetchAll($select);
+	return $data;
+	}
+	
+}
+
---
+++ app/models/VanArsdellTypes.php
@@ -0,0 +1,76 @@
+<?php
+/**
+* Data model for accessing treasure valuation dates and cases from link table
+* @category Zend
+* @package Db_Table
+* @subpackage Abstract
+* 
+* @author Daniel Pett dpett @ britishmuseum.org
+* @copyright 2010 - DEJ Pett
+* @license GNU General Public License
+* @version 1
+* @since 22 October 2010, 17:12:34
+* @todo integrate with the VanArsdellTypes
+*/
+
+class VanArsdellTypes extends Zend_Db_Table_Abstract {
+
+	protected $_name = 'vanarsdelltypes';
+
+	protected $_primary = 'id';
+
+	protected $_cache;
+	
+	/** Construct the cache objects
+	* @return object
+	*/
+	public function init() {
+	$this->_cache = Zend_Registry::get('rulercache');
+	}
+
+	/** Get a dropdown list of VA types as key value array
+	* @return array
+	*/
+	public function getVATypesDD() {
+    if (!$options = $this->_cache->load('vatypedd')) {
+	$select = $this->select()
+		->from($this->_name, array('type', 'type'))
+		->order('type');
+	$options = $this->getAdapter()->fetchPairs($select);
+	$this->_cache->save($options, 'vatypedd');
+	}
+	return $options;
+    }
+    
+    /** Get a list of types based on query string
+    * @param string $q 
+	* @return array
+	*/
+	public function getTypes($q) {
+	$types = $this->getAdapter();
+	$select = $types->select()
+		->from($this->_name, array('id','term' => 'type'))
+		->where('type LIKE ? ', (string)$q.'%')
+		->order('type')
+		->limit(10);
+	return $types->fetchAll($select);
+	}
+
+	/** Get a list of va types paginated
+    * @param integer $params['page'] page number requested 
+	* @return array
+	*/
+	public function getVaTypes($params) {
+	$types = $this->getAdapter();
+	$select = $types->select()
+		->from($this->_name);
+	$paginator = Zend_Paginator::factory($select);
+	$paginator->setItemCountPerPage(30) 
+		->setPageRange(20);
+	if(isset($params['page']) && ($params['page'] != "")) {
+    $paginator->setCurrentPageNumber($params['page']); 
+	}
+	return $paginator;
+	}
+
+}
