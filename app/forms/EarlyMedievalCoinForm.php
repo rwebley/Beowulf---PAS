d79628daa95e0ad1436840de69500abc83d944c1
unknown <dpett@.local>
2011-06-15 17:37:08 +0100

    Removed cruft

--- app/forms/EarlyMedievalCoinForm.php
+++ app/forms/EarlyMedievalCoinForm.php
@@ -1,5 +1,11 @@
 <?php
-
+/** Form for entering data about Early Medieval coins
+* 
+* @category   Pas
+* @package    Pas_Form
+* @copyright  Copyright (c) 2011 DEJ Pett dpett @ britishmuseum . org
+* @license    GNU General Public License
+*/
 class EarlyMedievalCoinForm extends Pas_Form {
 	
 	public function __construct($options = null) {
@@ -13,7 +19,6 @@ class EarlyMedievalCoinForm extends Pas_Form {
 	
 	$statuses = new Statuses();
 	$status_options = $statuses->getCoinStatus();
-	
 	
 	$dies = new Dieaxes();
 	$die_options = $dies->getAxes();
@@ -29,7 +34,6 @@ class EarlyMedievalCoinForm extends Pas_Form {
 	
 	parent::__construct($options);
 	
-	       
 	$this->setName('earlymedievalcoin');
 	$decorators = array(
             array('ViewHelper'), 
@@ -39,7 +43,6 @@ class EarlyMedievalCoinForm extends Pas_Form {
             array('HtmlTag', array('tag' => 'li')),
 		    );
 		
-
 	$denomination = new Zend_Form_Element_Select('denomination');
 	$denomination->setLabel('Denomination: ')
 				 ->addValidators(array('NotEmpty'))
@@ -49,29 +52,33 @@ class EarlyMedievalCoinForm extends Pas_Form {
 	$denomination_qualifier = new Zend_Form_Element_Radio('denomination_qualifier');
 	$denomination_qualifier->setLabel('Denomination qualifier: ')
 							->addMultiOptions(array('1' => 'Certain','2' => 'Probably','3' => 'Possibly'))
-							->addFilter('StripTags')
-							->addFilter('StringTrim')
+							->addFilters(array('StripTags','StringTrim'))
 							->setOptions(array('separator' => ''))
 							->addDecorator('HtmlTag', array('placement' => 'prepend','tag'=>'div','id'=>'radios'))
-							->setDecorators($decorators);
+							->setDecorators($decorators)
+							->addValidator('Int');
 	
 	$categoryID = new Zend_Form_Element_Select('categoryID');
 	$categoryID->setLabel('Category of coin: ')
 				->addValidators(array('NotEmpty'))
 				->addMultiOptions(array(NULL => NULL,'Choose category' => $cat_options))
-				->setDecorators($decorators);
+				->setDecorators($decorators)
+				->addFilters(array('StripTags','StringTrim'))
+				->addValidator('Int');
 	
 	$ruler_id= new Zend_Form_Element_Select('ruler');
 	$ruler_id->setLabel('Ruler: ')
 			->setRegisterInArrayValidator(false)
 			->setDecorators($decorators)
-			->addMultiOptions(array(NULL => NULL,'Please choose a ruler' => $ro));
+			->addMultiOptions(array(NULL => NULL,'Please choose a ruler' => $ro))
+			->addValidator('Int')
+			->addFilters(array('StripTags','StringTrim'));
 			
 	$ruler_qualifier = new Zend_Form_Element_Radio('ruler_qualifier');
 	$ruler_qualifier->setLabel('Issuer qualifier: ')
 					->addMultiOptions(array('1' => 'Certain','2' => 'Probably','3' => 'Possibly'))
-					->addFilter('StripTags')
-					->addFilter('StringTrim')
+					->addFilters(array('StripTags','StringTrim'))
+					->addValidator('Int')
 					->setOptions(array('separator' => ''))
 					->addDecorator('HtmlTag', array('placement' => 'prepend','tag'=>'div','id'=>'radios'))
 					->setDecorators($decorators);
@@ -80,8 +87,8 @@ class EarlyMedievalCoinForm extends Pas_Form {
 	$mint_id->setLabel('Issuing mint: ')
 			->setRegisterInArrayValidator(false)
 			->setDecorators($decorators)
-			->addFilter('StripTags')
-			->addFilter('StringTrim')
+			->addFilters(array('StripTags','StringTrim'))
+			->addValidator('Int')
 			->addMultiOptions(array(NULL => NULL,'Please choose a mint' => $mo));
 	
 	
@@ -89,8 +96,8 @@ class EarlyMedievalCoinForm extends Pas_Form {
 	$status->setLabel('Status: ')
 			->setRegisterInArrayValidator(false)
 			->setValue(1)
-			->addFilter('StripTags')
-			->addFilter('StringTrim')
+			->addFilters(array('StripTags','StringTrim'))
+			->addValidator('Int')
 			->addMultiOptions(array(NULL => NULL,'Choose coin status' => $status_options))
 			->setDecorators($decorators);
 	
@@ -98,8 +105,8 @@ class EarlyMedievalCoinForm extends Pas_Form {
 	$status_qualifier->setLabel('Status qualifier: ')
 					->addMultiOptions(array('1' => 'Certain','2' => 'Probably','3' => 'Possibly'))
 					->setValue(1)
-					->addFilter('StripTags')
-					->addFilter('StringTrim')
+					->addFilters(array('StripTags','StringTrim'))
+					->addValidator('Int')
 					->setOptions(array('separator' => ''))
 					->addDecorator('HtmlTag', array('placement' => 'prepend','tag'=>'div','id'=>'radios'))
 					->setDecorators($decorators);
@@ -110,37 +117,36 @@ class EarlyMedievalCoinForm extends Pas_Form {
 					->setRegisterInArrayValidator(false)
 					->addMultiOptions(array(NULL => NULL,'Choose coin status' => $wear_options))
 					->setDecorators($decorators)
-					->addFilter('StripTags')
-					->addFilter('StringTrim');
+					->addFilters(array('StripTags','StringTrim'))
+					->addValidator('Int');
 	
 	$obverse_inscription = new Zend_Form_Element_Text('obverse_inscription');
 	$obverse_inscription->setLabel('Obverse inscription: ')
 						->setDecorators($decorators)
-						->setAttrib('size',60);
+						->setAttrib('size',60)
+						->addFilters(array('StripTags','StringTrim'));
 	
 	$reverse_inscription = new Zend_Form_Element_Text('reverse_inscription');
 	$reverse_inscription->setLabel('Reverse inscription: ')
 						->setDecorators($decorators)
-						->setAttrib('size',60);
+						->setAttrib('size',60)
+						->addFilters(array('StripTags','StringTrim'));
 						
 	$obverse_description = new Zend_Form_Element_Textarea('obverse_description');
 	$obverse_description->setLabel('Obverse description: ')
 						->addValidators(array('NotEmpty'))
 						->setAttrib('rows',3)
 						->setAttrib('cols',80)
-						->addFilter('StripTags')
-						->addFilter('EmptyParagraph')
-						->addFilter('StringTrim')
-						->setAttrib('class','expanding');
+						->addFilters(array('StripTags','EmptyParagraph','StringTrim'))
+						->setAttrib('class','expanding')
+						->addFilters(array('StripTags','StringTrim'));
 	
 	$reverse_description = new Zend_Form_Element_Textarea('reverse_description');
 	$reverse_description->setLabel('Reverse description: ')
 						->addValidators(array('NotEmpty'))
 						->setAttrib('rows',3)
 						->setAttrib('cols',80)
-						->addFilter('StripTags')
-						->addFilter('EmptyParagraph')
-						->addFilter('StringTrim')
+						->addFilters(array('StripTags','EmptyParagraph','StringTrim'))
 						->setAttrib('class','expanding');
 	
 	$rev_mm = new Zend_Form_Element_Textarea('reverse_mintmark');
@@ -164,14 +170,14 @@ class EarlyMedievalCoinForm extends Pas_Form {
 	$die_axis_measurement = new Zend_Form_Element_Select('die_axis_measurement');
 	$die_axis_measurement->setLabel('Die axis measurement: ')
 						->setRegisterInArrayValidator(false)
+						->addFilters(array('StripTags','StringTrim'))
 						->addMultiOptions(array(NULL => NULL,'Choose coin status' => $die_options))
 						->setDecorators($decorators);
 	
 	$die_axis_certainty = new Zend_Form_Element_Radio('die_axis_certainty');
 	$die_axis_certainty->setLabel('Die axis certainty: ')
 						->addMultiOptions(array('1' => 'Certain','2' => 'Probably','3' => 'Possibly'))
-						->addFilter('StripTags')
-						->addFilter('StringTrim')
+						->addFilters(array('StripTags','StringTrim'))
 						->setOptions(array('separator' => ''))
 						->addDecorator('HtmlTag', array('placement' => 'prepend','tag'=>'div','id'=>'radios'))
 						->setDecorators($decorators);
@@ -180,8 +186,7 @@ class EarlyMedievalCoinForm extends Pas_Form {
 	$typeID->setLabel('Coin type: ')
 			->setRegisterInArrayValidator(false)
 			->setRequired(false)
-			->addFilter('StripTags')
-			->addFilter('StringTrim')
+			->addFilters(array('StripTags','StringTrim'))
 			->setDecorators($decorators);
 	
 	$submit = new Zend_Form_Element_Submit('submit');
@@ -190,6 +195,13 @@ class EarlyMedievalCoinForm extends Pas_Form {
 				  ->removeDecorator('DtDdWrapper')
 				  ->setAttrib('class','large');
 	
+	$hash = new Zend_Form_Element_Hash('csrf');
+	$hash->setValue($_formsalt)
+	->removeDecorator('DtDdWrapper')
+	->removeDecorator('HtmlTag')->removeDecorator('label')
+	->setTimeout(60);
+	$this->addElement($hash);
+				  
 	$this->addElements(array( $ruler_id, $ruler_qualifier, $denomination,
 							  $denomination_qualifier, $mint_id, $typeID, 
 							  $status, $categoryID, $status_qualifier, 
@@ -218,6 +230,5 @@ class EarlyMedievalCoinForm extends Pas_Form {
 		$mint_options = $mints->getEarlyMedievalMints();
 		$mint_id->addMultiOptions(array(NULL => NULL,'Choose Medieval mint' => $mint_options));
 	}
-	
 	}
 }
