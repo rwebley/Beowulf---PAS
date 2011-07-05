c44a640d8fef85f3937e31f11c7323edd41dba72
unknown <dpett@.local>
2011-07-05 14:51:13 +0100

    Added more forms only 38 to go!

--- app/forms/PublicationForm.php
+++ app/forms/PublicationForm.php
@@ -1,143 +1,150 @@
 <?php
+/** Form for setting up and editing publications data
+* @category   Pas
+* @package    Pas_Form
+* @copyright  Copyright (c) 2011 DEJ Pett dpett @ britishmuseum . org
+* @license    GNU General Public License
+*/
+class PublicationForm extends Pas_Form {
 
-class PublicationForm extends Pas_Form
-{
-
-public function __construct($options = null)
-{
-$types = new Publicationtypes();
-$type_options = $types->getTypes();
-
-parent::__construct($options);
-
-$decorators = array(
-            array('ViewHelper'), 
-            array('Description', array('placement' => 'append','class' => 'info')),
-            array('Errors',array('placement' => 'append','class'=>'error','tag' => 'li')),
-            array('Label'),
-            array('HtmlTag', array('tag' => 'li')),
-		    );
-$this->setName('publication');
-
-
-
-$title = new Zend_Form_Element_Text('title');
-$title->setLabel('Publication title: ')
-->setRequired(true)
-->setAttrib('size',50)
-->addErrorMessage('Please enter a publication title.')
-->setDecorators($decorators);
-
-$authors = new Zend_Form_Element_Text('authors');
-$authors->setLabel('Author names: ')
-->setRequired(true)
-->setAttrib('size',50)
-->addErrorMessage('You must enter either an author\'s or an editor\'s name.')
-->setDecorators($decorators);
-
-
-$editors = new Zend_Form_Element_Text('editors');
-$editors->setLabel('Editor names: ')
-->setRequired(false)
-->setAttrib('size',50)
-->setDecorators($decorators);
-
-
-$publisher = new Zend_Form_Element_Text('publisher');
-$publisher->setLabel('Publisher: ')
-->setRequired(true)
-->setAttrib('size',50)
-->addErrorMessage('You must enter a publisher.')
-->setDecorators($decorators);
-
-$publication_place = new Zend_Form_Element_Text('publication_place');
-$publication_place->setLabel('Publication place: ')
-->setRequired(true)
-->setAttrib('size',70)
-->addErrorMessage('You must enter place of publication.')
-->setDecorators($decorators);
-
-
-$publication_year = new Zend_Form_Element_Text('publication_year');
-$publication_year->setLabel('Publication year: ')
-->setRequired(true)
-->setAttrib('size',20)
-->addErrorMessage('You must enter year of publication.')
-->setDecorators($decorators);
-
-
-$vol_no = new Zend_Form_Element_Text('vol_no');
-$vol_no->setLabel('Volume number: ')
-->setRequired(false)
-->setAttrib('size',20)
-->setDecorators($decorators);
-
-
-$edition = new Zend_Form_Element_Text('edition');
-$edition->setLabel('Edition: ')
-->setRequired(false)
-->setAttrib('size',20)
-->setDecorators($decorators);
-
-
-
-$in_publication = new Zend_Form_Element_Text('in_publication');
-$in_publication->setLabel('In publication: ')
-->setRequired(false)
-->setDecorators($decorators);
-
-
-$publication_type = new Zend_Form_Element_Select('publication_type');
-$publication_type->setLabel('Publication type: ')
-->setRequired(false)
-->addFilter('StripTags')
-->addFilter('StringTrim')
-->addMultiOptions(array(NULL,'Choose reason' => $type_options))
-//->addValidator('inArray', false, array(array_keys($type_options)))
-->setDecorators($decorators);
-
-$ISBN = new Zend_Form_Element_Text('ISBN');
-$ISBN->setLabel('ISBN (allows people to look it up on Amazon): ')
-->setRequired(false)
-->setAttrib('size',40)
-->addValidator('Isbn')
-->setDecorators($decorators);
-
-//Submit button 
-$submit = new Zend_Form_Element_Submit('submit');
-$submit->setAttrib('id', 'submitbutton')
-		->removeDecorator('label')
-        ->removeDecorator('HtmlTag')
-		->removeDecorator('DtDdWrapper')
-		->setAttrib('class','large');
-
-$this->addElements(array(
-$title,
-$authors,
-$publisher,
-$publication_place,
-$publication_year,
-$vol_no,
-$edition,
-$in_publication,
-$editors,
-$publication_type,
-$ISBN,
-$submit));
-
-$this->addDisplayGroup(array('title','authors','publisher','publication_place','publication_year','vol_no','edition','in_publication','editors','publication_type','ISBN'), 'details')->removeDecorator('HtmlTag');
-$this->details->addDecorators(array('FormElements',array('HtmlTag', array('tag' => 'ul'))));
-$this->details->removeDecorator('DtDdWrapper');
-$this->details->removeDecorator('HtmlTag');
-
-$this->addDisplayGroup(array('submit'), 'submit')->removeDecorator('HtmlTag');
-$this->submit->removeDecorator('DtDdWrapper');
-$this->submit->removeDecorator('HtmlTag');
-
-$this->details->setLegend('Publication details: ');
-
-
-      
-
-}
+public function __construct($options = null) {
+	$types = new Publicationtypes();
+	$type_options = $types->getTypes();
+	
+	parent::__construct($options);
+	
+	$decorators = array(
+	            array('ViewHelper'), 
+	            array('Description', array('placement' => 'append','class' => 'info')),
+	            array('Errors',array('placement' => 'append','class'=>'error','tag' => 'li')),
+	            array('Label'),
+	            array('HtmlTag', array('tag' => 'li')),
+			    );
+			    
+	$this->setName('publication');
+	
+	$title = new Zend_Form_Element_Text('title');
+	$title->setLabel('Publication title: ')
+		->setRequired(true)
+		->addFilters(array('StripTags','StringTrim'))
+		->addValidator('Alnum',false,array('allowWhiteSpace' => true))
+		->setAttrib('size',50)
+		->addErrorMessage('Please enter a publication title.')
+		->setDecorators($decorators);
+	
+	$authors = new Zend_Form_Element_Text('authors');
+	$authors->setLabel('Author names: ')
+		->setRequired(true)
+		->addFilters(array('StripTags','StringTrim'))
+		->addValidator('Alnum',false,array('allowWhiteSpace' => true))
+		->setAttrib('size',50)
+		->addErrorMessage('You must enter either an author\'s or an editor\'s name.')
+		->setDecorators($decorators);
+	
+	$editors = new Zend_Form_Element_Text('editors');
+	$editors->setLabel('Editor names: ')
+		->setRequired(false)
+		->addValidator('Alnum',false,array('allowWhiteSpace' => true))
+		->addFilters(array('StripTags','StringTrim'))
+		->setAttrib('size',50)
+		->setDecorators($decorators);
+	
+	$publisher = new Zend_Form_Element_Text('publisher');
+	$publisher->setLabel('Publisher: ')
+		->addFilters(array('StripTags','StringTrim'))
+		->setRequired(true)
+		->addValidator('Alnum',false,array('allowWhiteSpace' => true))
+		->setAttrib('size',50)
+		->addErrorMessage('You must enter a publisher.')
+		->setDecorators($decorators);
+	
+	$publication_place = new Zend_Form_Element_Text('publication_place');
+	$publication_place->setLabel('Publication place: ')
+		->addFilters(array('StripTags','StringTrim'))
+		->setRequired(true)
+		->addValidator('Alnum',false,array('allowWhiteSpace' => true))
+		->setAttrib('size',70)
+		->addErrorMessage('You must enter place of publication.')
+		->setDecorators($decorators);
+	
+	$publication_year = new Zend_Form_Element_Text('publication_year');
+	$publication_year->setLabel('Publication year: ')
+		->setRequired(true)
+		->addValidator('Digits')
+		->addFilters(array('StripTags','StringTrim'))
+		->setAttrib('size',20)
+		->addErrorMessage('You must enter year of publication.')
+		->setDecorators($decorators);
+	
+	$vol_no = new Zend_Form_Element_Text('vol_no');
+	$vol_no->setLabel('Volume number: ')
+		->setRequired(false)
+		->addValidator('Alnum',false,array('allowWhiteSpace' => true))
+		->setAttrib('size',20)
+		->addFilters(array('StripTags','StringTrim'))
+		->setDecorators($decorators);
+	
+	$edition = new Zend_Form_Element_Text('edition');
+	$edition->setLabel('Edition: ')
+		->setRequired(false)
+		->addValidator('Alnum',false,array('allowWhiteSpace' => true))
+		->addFilters(array('StripTags','StringTrim'))
+		->setAttrib('size',20)
+		->setDecorators($decorators);
+	
+	$in_publication = new Zend_Form_Element_Text('in_publication');
+	$in_publication->setLabel('In publication: ')
+		->addFilters(array('StripTags','StringTrim'))
+		->setRequired(false)
+		->addValidator('Alnum',false,array('allowWhiteSpace' => true))
+		->setDecorators($decorators);
+	
+	$publication_type = new Zend_Form_Element_Select('publication_type');
+	$publication_type->setLabel('Publication type: ')
+		->setRequired(false)
+		->addFilters(array('StripTags','StringTrim'))
+		->addMultiOptions(array(NULL,'Choose reason' => $type_options))
+		->addValidator('InArray', false, array(array_keys($type_options)))
+		->setDecorators($decorators);
+	
+	$ISBN = new Zend_Form_Element_Text('ISBN');
+	$ISBN->setLabel('ISBN (allows people to look it up on Amazon): ')
+		->setRequired(false)
+		->addFilters(array('StripTags','StringTrim'))
+		->setAttrib('size',40)
+		->addValidator('Isbn')
+		->setDecorators($decorators);
+	
+	//Submit button 
+	$submit = new Zend_Form_Element_Submit('submit');
+	$submit->setAttrib('id', 'submitbutton')
+			->removeDecorator('label')
+	        ->removeDecorator('HtmlTag')
+			->removeDecorator('DtDdWrapper')
+			->setAttrib('class','large');
+	
+	$this->addElements(array(
+	$title, $authors, $publisher,
+	$publication_place,	$publication_year, 	$vol_no,
+	$edition,$in_publication, $editors,
+	$publication_type, $ISBN, $submit)
+	);
+	
+	$this->addDisplayGroup(array(
+	'title','authors','publisher',
+	'publication_place','publication_year','vol_no',
+	'edition','in_publication','editors',
+	'publication_type','ISBN'), 'details')
+	->removeDecorator('HtmlTag');
+	$this->details->addDecorators(array('FormElements',array('HtmlTag', array('tag' => 'ul'))));
+	$this->details->removeDecorator('DtDdWrapper');
+	$this->details->removeDecorator('HtmlTag');
+	
+	$this->addDisplayGroup(array('submit'), 'submit')->removeDecorator('HtmlTag');
+	$this->submit->removeDecorator('DtDdWrapper');
+	$this->submit->removeDecorator('HtmlTag');
+	
+	$this->details->setLegend('Publication details: ');
+	
+	}
 }
--- app/forms/PublishCommentFindForm.php
+++ app/forms/PublishCommentFindForm.php
@@ -1,124 +1,118 @@
 <?php
-class PublishCommentFindForm extends Pas_Form
-{
+/** Form for publishing comments on finds
+* @category   Pas
+* @package    Pas_Form
+* @copyright  Copyright (c) 2011 DEJ Pett dpett @ britishmuseum . org
+* @license    GNU General Public License
+*/
+class PublishCommentFindForm extends Pas_Form {
 
+public function __construct($options = null) {
 
+	parent::__construct($options);
 
-public function __construct($options = null)
-{
-
-parent::__construct($options);
-
-$decorators = array(
+	$decorators = array(
             array('ViewHelper'), 
     		array('Description', array('tag' => '','placement' => 'append')),
             array('Errors',array('placement' => 'append','tag' => 'li')),
             array('Label', array('separator'=>' ', 'requiredSuffix' => ' *')),
             array('HtmlTag', array('tag' => 'li')),
 		    );
-			
+	
+	$this->setName('comments');
+	
+	$comment_author_IP = new Zend_Form_Element_Hidden('comment_author_IP');
+	$comment_author_IP->removeDecorator('HtmlTag')
+		->addFilters(array('StripTags','StringTrim'))
+		->removeDecorator('DtDdWrapper')
+		->removeDecorator('Label')
+		->setValue($_SERVER['REMOTE_ADDR'])
+		->addValue('Ip');
 
-$this->setName('comments');
+	$comment_agent = new Zend_Form_Element_Hidden('comment_agent');
+	$comment_agent->removeDecorator('HtmlTag')
+		->removeDecorator('DtDdWrapper')
+		->removeDecorator('Label')
+		->setValue($_SERVER['HTTP_USER_AGENT'])
+		->setRequired(false)
+		->addFilters(array('StripTags','StringTrim'));
 
-$comment_author_IP = new Zend_Form_Element_Hidden('comment_author_IP');
-$comment_author_IP->removeDecorator('HtmlTag')
-->removeDecorator('DtDdWrapper')
-->removeDecorator('Label');
+	$comment_findID = new Zend_Form_Element_Hidden('comment_findID');
+	$comment_findID->addFilters(array('StripTags','StringTrim'))
+		->setDecorators(array(
+	    array('ViewHelper'),
+	    array('Description', array('tag' => '')),
+	    array('Errors'),
+	    array('HtmlTag', array('tag' => 'p')),
+	    array('Label', array('tag' => ''))
+	));
+	
+	$comment_author = new Zend_Form_Element_Text('comment_author');
+	$comment_author->setLabel('Enter your name: ')
+		->setRequired(true)
+		->addFilters(array('StripTags','StringTrim'))
+		->addValidator('Alnum',false,array('allowWhiteSpace' => true))
+		->addErrorMessage('Please enter a valid name!')
+		->setDecorators($decorators);
 
-$comment_author_IP->setValue($_SERVER['REMOTE_ADDR']);
+	$comment_author_email = new Zend_Form_Element_Text('comment_author_email');
+	$comment_author_email->setLabel('Enter your email address: ')
+		->setDecorators($decorators)
+		->setRequired(true)
+		->addFilters(array('StripTags','StringTrim','StringToLower'))
+		->addValidator('EmailAddress',false,array('mx' => true))   
+		->addErrorMessage('Please enter a valid email address!')
+		->setDescription('* This will not be displayed to the public.');
 
-$comment_agent = new Zend_Form_Element_Hidden('comment_agent');
-$comment_agent->removeDecorator('HtmlTag')
-->removeDecorator('DtDdWrapper')
-->removeDecorator('Label');
-
-$comment_agent->setValue($_SERVER['HTTP_USER_AGENT'])
-->setRequired(false);
-
-
-
-$comment_findID = new Zend_Form_Element_Hidden('comment_findID');
-$comment_findID->setDecorators(array(
-    array('ViewHelper'),
-    array('Description', array('tag' => '')),
-    array('Errors'),
-    array('HtmlTag', array('tag' => 'p')),
-    array('Label', array('tag' => ''))
-));
-
-$comment_author = new Zend_Form_Element_Text('comment_author');
-$comment_author->setLabel('Enter your name: ')
-->setRequired(true)
-->addFilter('StripTags')
-->addFilter('StringTrim')
-->addValidator('NotEmpty')
-->addErrorMessage('Please enter a valid name!')
-->setDecorators($decorators);
-
-$comment_author_email = new Zend_Form_Element_Text('comment_author_email');
-$comment_author_email->setLabel('Enter your email address: ')
-->setDecorators($decorators)
-->setRequired(true)
-->addFilter('StripTags')
-->addFilter('StringTrim')
-->addValidator('EmailAddress')   
-->addFilter('StringToLower')
-->addErrorMessage('Please enter a valid email address!')
-->setDescription('* This will not be displayed to the public.');
-
-$comment_author_url = new Zend_Form_Element_Text('comment_author_url');
-$comment_author_url->setLabel('Enter your web address: ')
-->setDecorators($decorators)
-->setRequired(false)
-->addFilter('StripTags')
-->addFilter('StringTrim')
-->addValidator('NotEmpty')
-->addFilter('StringToLower')
-->addErrorMessage('Please enter a valid address!')
-->setDescription('* Not compulsory');
+	$comment_author_url = new Zend_Form_Element_Text('comment_author_url');
+	$comment_author_url->setLabel('Enter your web address: ')
+		->setDecorators($decorators)
+		->setRequired(false)
+		->addFilters(array('StripTags','StringTrim','StringToLower'))
+		->addErrorMessage('Please enter a valid address!')
+		->addValidator('Url')
+		->setDescription('* Not compulsory');
 
 
-$comment_content = new Pas_Form_Element_RTE('comment_content');
-$comment_content->setLabel('Enter your comment: ')
-->setRequired(true)
-->addFilter('StringTrim')
-->setAttrib('rows',10)
-->setAttrib('cols',80)
-->addFilter('BasicHtml')
-->addErrorMessage('Please enter something in the comments box.');
+	$comment_content = new Pas_Form_Element_RTE('comment_content');
+	$comment_content->setLabel('Enter your comment: ')
+		->setRequired(true)
+		->setAttrib('rows',10)
+		->setAttrib('cols',40)
+		->setAttrib('Height',400)
+		->setAttrib('ToolbarSet','Finds')
+		->addFilters(array('StringTrim', 'BasicHtml', 'EmptyParagraph', 'WordChars'));
 
-$submit = new Zend_Form_Element_Submit('submit');
-$submit->setAttrib('id', 'submitbutton')->removeDecorator('label')
-              ->removeDecorator('HtmlTag')
-			  ->removeDecorator('DtDdWrapper')
-			  ->setAttrib('class', 'large');
-$approval = new Zend_Form_Element_Radio('approval');
-$approval->setLabel('What would you like to do? ')
-->addMultiOptions(array('spam' => 'Set as spam','ham' => 'Submit ham?','approved' => 'Publish it?','delete' => 'Delete it?'))
-->setValue('approved')
-->addFilter('StripTags')
-->addFilter('StringTrim')
-->setOptions(array('separator' => ''))
-->setDecorators($decorators);			  
+	$submit = new Zend_Form_Element_Submit('submit');
+	$submit->setAttrib('id', 'submitbutton')
+		->removeDecorator('label')
+		->removeDecorator('HtmlTag')
+		->removeDecorator('DtDdWrapper')
+		->setAttrib('class', 'large');
+		
+	$approval = new Zend_Form_Element_Radio('approval');
+	$approval->setLabel('What would you like to do? ')
+		->addMultiOptions(array('spam' => 'Set as spam','ham' => 'Submit ham?','approved' => 'Publish it?','delete' => 'Delete it?'))
+		->setValue('approved')
+		->addFilters(array('StripTags','StringTrim','StringToLower'))
+		->setOptions(array('separator' => ''))
+		->setDecorators($decorators);			  
 
-$this->addElements(array(
-$comment_author_IP,
-$comment_agent,
-$comment_author,
-$comment_author_email,
-$comment_content,
-$comment_author_url,
-$comment_findID,
-$approval,
-$submit));
+	$this->addElements(array(
+	$comment_author_IP, $comment_agent, $comment_author,
+	$comment_author_email, $comment_content, $comment_author_url,
+	$comment_findID, $approval, $submit)
+	);
 
-$this->addDisplayGroup(array('comment_author','comment_author_email','comment_author_url','comment_content','approval','comment_findID'), 'details');
-
-$this->details->addDecorators(array('FormElements',array('HtmlTag', array('tag' => 'ul'))));
-$this->details->removeDecorator('HtmlTag');
-$this->details->removeDecorator('DtDdWrapper');
-$this->details->setLegend('Enter your comments: ');
-
-$this->addDisplayGroup(array('submit'), 'submit');
-}
+	$this->addDisplayGroup(array(
+	'comment_author','comment_author_email','comment_author_url',
+	'comment_content','approval','comment_findID'), 'details');
+	
+	$this->details->addDecorators(array('FormElements',array('HtmlTag', array('tag' => 'ul'))));
+	$this->details->removeDecorator('HtmlTag');
+	$this->details->removeDecorator('DtDdWrapper');
+	$this->details->setLegend('Enter your comments: ');
+	
+	$this->addDisplayGroup(array('submit'), 'submit');
+	}
 }
