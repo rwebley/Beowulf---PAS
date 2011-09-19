301faca053d181ff77219a72ff326817a6257c7b
Daniel Pett <dpett@britishmuseum.org>
2011-09-19 15:34:40 +0100

    Added some more view helpers

---
+++ library/Pas/View/Helper/MetaBase.php
@@ -0,0 +1,38 @@
+<?php 
+/**
+ * A view helper to display the meta data for a page
+ * @category   Pas
+ * @package    Pas_View_Helper
+ * @subpackage Abstract
+ * @copyright  Copyright (c) 2011 dpett @ britishmuseum.org
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @see Zend_View_Helper_Abstract
+ */
+
+class Pas_View_Helper_MetaBase extends Zend_View_Helper_Abstract {
+
+ 	/** View helper to produce metadata for the head section
+ 	 * @access public
+ 	 * @param $description
+ 	 * @param $subject
+ 	 * @param $keywords array 
+ 	 */
+	public function metabase($description,$subject = 'archaeology', $keywords) {
+	$date = new Zend_Date();
+	$date->add('72',Zend_Date::HOUR);
+	$this->view->headMeta()
+		->appendHttpEquiv('expires',$date->get(Zend_Date::RFC_1123))
+		->appendHttpEquiv('Content-Type','text/html; charset=UTF-8')
+		->appendHttpEquiv('Content-Language', 'en-GB')
+		->appendHttpEquiv('imagetoolbar', 'no')
+		->headMeta($this->view->title(),'dc.title')
+		->headMeta($this->view->CurUrl(),'dc.identifier og:url')
+		->headMeta($keywords,'keywords')
+		->headMeta('The Portable Antiquities Scheme and the British Museum','dc.publisher')
+		->headMeta($this->view->escape($description),'description')
+		->headMeta()->setProperty('dc.subject',$this->view->escape($subject))
+		->headMeta()->setProperty('og:site_name','Portable Antiquities')
+		->headMeta()->setProperty('dc.rights','Creative Commons NC-BY-SA');
+	}
+
+}
---
+++ library/Pas/View/Helper/Query.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * A view helper for turning dates into Roman numerals
+ * @category   Pas
+ * @package    Pas_View_Helper
+ * @subpackage Abstract
+ * @copyright  Copyright (c) 2011 dpett @ britishmuseum.org
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @see Zend_View_Helper_Abstract
+ * @author Daniel Pett
+ */
+class Pas_View_Helper_Query extends Zend_View_Helper_Abstract {
+
+	/** Create and print the query string separated by /
+	 * @access public
+	 * @param array $params the array of params taken from the request
+	 * @return string $query The formatted query string
+	 */
+	public function Query($params) {
+		unset($params['controller']);
+		unset($params['module']);
+		unset($params['action']);
+		unset($params['submit']);
+		$where = array();
+        foreach($params as $key => $value)
+        {
+			if($value != NULL){
+            $where[] = $key . '/' . urlencode($value);
+			}
+        }
+   	$whereString = implode('/', $where);
+	$query = $whereString;
+	return $query;
+	}
+}
