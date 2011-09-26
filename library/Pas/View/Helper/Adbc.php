dafe9784e9b520c7569a699152c32b082e16798d
Daniel Pett <dpett@britishmuseum.org>
2011-09-26 18:12:09 +0100

    Removed code from OAI and switched to PAS geo

--- library/Pas/View/Helper/Adbc.php
+++ library/Pas/View/Helper/Adbc.php
@@ -1,17 +1,19 @@
 <?php 
 /**
  * A view helper for turning dates into AD or BC calenderical dates 
- * @category   Pas
- * @package    Pas_View_Helper
- * @subpackage Abstract
- * @copyright  Copyright (c) 2011 dpett @ britishmuseum.org
- * @license    http://framework.zend.com/license/new-bsd     New BSD License
- * @see Zend_View_Helper_Abstract
+ * @category   	Pas
+ * @package    	Pas_View_Helper
+ * @subpackage 	Abstract
+ * @copyright  	Copyright (c) 2011 dpett @ britishmuseum.org
+ * @license    	GNU
+ * @see 		Zend_View_Helper_Abstract
+ * @author     	Originally TW Bell, extended for Zend by Daniel Pett
+ * @version		1
+ * @since		26 September 2011
  */
 class Pas_View_Helper_Adbc extends Zend_View_Helper_Abstract {
 	
-	/**
-	 *  A function for turning the integer into a string with AD or BC added
+	/** A function for turning the integer into a string with AD or BC added
 	 * 
 	 * @param $date integer 
 	 * @param $suffix string
