28945c10874427677ac289ac59b90cfd9329c2b7
Daniel Pett <dpett@britishmuseum.org>
2011-10-04 16:26:24 +0100

    Updated view scripts

--- app/modules/admin/controllers/ConfigurationController.php
+++ app/modules/admin/controllers/ConfigurationController.php
@@ -13,13 +13,13 @@ class Admin_ConfigurationController extends Pas_Controller_Action_Admin {
 	/** Set up the ACL and contexts
 	*/		
 	public function init() {
- 		$this->_helper->_acl->allow('admin',null);
-        $this->_flashMessenger = $this->_helper->getHelper('FlashMessenger');
-    }
+	$this->_helper->_acl->allow('admin',null);
+	$this->_flashMessenger = $this->_helper->getHelper('FlashMessenger');
+	}
+	
 	/** Display the index page
 	*/		
 	public function indexAction(){
-	
 	}
 	/** Display the webservice configurations
 	 * 
@@ -28,15 +28,24 @@ class Admin_ConfigurationController extends Pas_Controller_Action_Admin {
 	$this->view->webservice = $this->_config->webservice->toArray();
 	}
 	/** Display the system configurations
-	 * 
 	 */
 	public function systemAction(){
 	$this->view->resources = $this->_config->resources->toArray();	
 	}
-	
+	/** Display the routing configurations
+	*/
 	public function routingAction(){
 	$config = new Zend_Config_Ini('app/config/routes.ini', 'production');
 	$this->view->routing = $config->toArray();	
+	}
+	
+	public function aclAction(){
+	$this->view->acl = $this->_config->acl->toArray();	
+	}
+	
+	public function saltsAction(){
+	$this->view->salt = $this->_config->form->salt;
+	$this->view->authority = $this->_config->auth->salt;	
 	}
 }
 	
