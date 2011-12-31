e1113597ee4c6eca8c6c7a87a5ab68f0760d3e71
Daniel Pett <dpett@britishmuseum.org>
2011-12-31 13:41:42 +0000

    Removed method from finds

--- app/models/Acronyms.php
+++ app/models/Acronyms.php
@@ -13,45 +13,48 @@
 class Acronyms 
 	extends Pas_Db_Table_Abstract {
 
-	protected $_primary = 'id';
-	protected $_name = 'abbreviations';
+    protected $_primary = 'id';
+    protected $_name = 'abbreviations';
 	
-	/** Get all valid acronyms
-	* @access public
-	* @return array
-	*/
-	public function getValid() {
-		if (!$data = $this->_cache->load('acronymsSite')) {
-		$acros = $this->getAdapter();
-		$select = $acros->select()
-			->from($this->_name, array('abbreviation','expanded'))
-			->where('valid = 1');
-		$data =  $acros->fetchPairs($select); 
-		$this->_cache->save($data, 'acronymsSite');
-		}
-	return $data;
-	}
+    /** Get all valid acronyms
+    * @access public
+    * @return array
+    */
+    public function getValid() {
+    if (!$data = $this->_cache->load('acronymsSite')) {
+    $acros = $this->getAdapter();
+    $select = $acros->select()
+        ->from($this->_name, array('abbreviation','expanded'))
+	->where('valid = 1');
+    $data =  $acros->fetchPairs($select); 
+    $this->_cache->save($data, 'acronymsSite');
+    }
+    return $data;
+    }
 	
-	/** Get list of all acronyms and paginator them
-	* @access public
-	* @return array
-	* @param  array $params sent via controller
-	*/
-	public function getAllAcronyms($params)	{
-		$acros = $this->getAdapter();
-		$select = $acros->select()
-			->from($this->_name, array('id', 'abbreviation', 'expanded', 'updated'))
-			->joinLeft('users','users.id = ' . $this->_name . '.createdBy', array('fullname'))
-			->joinLeft('users','users_2.id = ' . $this->_name . '.updatedBy', array('fn' => 'fullname'))
-			->order('abbreviation');
-		$paginator = Zend_Paginator::factory($select);
-		$paginator->setItemCountPerPage(20) 
-		->setPageRange(20)
-		->setCache( $this->_cache );
-		if(isset($params['page']) && ($params['page'] != "")) {
-	    $paginator->setCurrentPageNumber($params['page']); 
-		}
-	return $paginator;
-	}
-
+    /** Get list of all acronyms and paginator them
+    * @access public
+    * @return array
+    * @param  array $params sent via controller
+    */
+    public function getAllAcronyms($params)	{
+    $acros = $this->getAdapter();
+    $select = $acros->select()
+	->from($this->_name, array(
+            'id', 'abbreviation', 'expanded', 
+            'updated'))
+	->joinLeft('users','users.id = ' . $this->_name . '.createdBy', 
+                array('fullname'))
+	->joinLeft('users','users_2.id = ' . $this->_name . '.updatedBy', 
+                array('fn' => 'fullname'))
+	->order('abbreviation');
+    $paginator = Zend_Paginator::factory($select);
+    $paginator->setItemCountPerPage(20) 
+	->setPageRange(20)
+	->setCache( $this->_cache );
+    if(isset($params['page']) && ($params['page'] != "")) {
+    $paginator->setCurrentPageNumber($params['page']); 
+    }
+    return $paginator;
+    }
 }
